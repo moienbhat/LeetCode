2777d2d59c90188a3f9851d4b9caed3d8232c38a
Moien Bhat <bhatmoien34@gmail.com>
2025-11-11 21:12:26 +0530

    Add files via upload

---
+++ 101SymmetricTree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isMirror(TreeNode* a, TreeNode* b) {
+        if (!a || !b) return a == b;
+
+        return (a->val == b->val)
+            && isMirror(a->left, b->right)
+            && isMirror(a->right, b->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return isMirror(root->left, root->right);
+    }
+};
